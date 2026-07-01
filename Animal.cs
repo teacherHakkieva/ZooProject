ee6eb32abdbd7204ea6a973a8c5664776f8abc1f
Teacher <teacher.hakkieva@gmail.com>
2026-07-01 22:04:47 +0300

    Create class Animal(fields)

---
+++ Animal.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZooProject
+{
+    public class Animal
+    {
+        public string AnimalID { get; set; }
+        public string Species { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Habitat { get; set; }
+        public bool Availability { get; set; }
+    }
+}
